9c5063bdde829cf0e00ff964471e644d6b27b526
SolidHal <hal@halemmerich.com>
2022-01-23 16:56:33 +0000

    add presigned versions of the stubs

--- gappstub/Android.mk
+++ gappstub/Android.mk
@@ -2,8 +2,18 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := gappsstub
+LOCAL_SRC_FILES := gappsstub.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := presigned
+include $(BUILD_PREBUILT)
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := gappsstub-unsigned
 LOCAL_SRC_FILES := gappsstub-unsigned.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_CERTIFICATE := releasekey
+LOCAL_CERTIFICATE := release
 include $(BUILD_PREBUILT)
--- speechservicestub/Android.mk
+++ speechservicestub/Android.mk
@@ -2,8 +2,18 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := speechservicestub
+LOCAL_SRC_FILES := speech-services-stub.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := presigned
+include $(BUILD_PREBUILT)
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := speechservicestub-unsigned
 LOCAL_SRC_FILES := speech-services-unsigned-stub.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_CERTIFICATE := releasekey
+LOCAL_CERTIFICATE := release
 include $(BUILD_PREBUILT)
